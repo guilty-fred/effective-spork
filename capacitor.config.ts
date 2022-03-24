dbb8f7590cb5fab7b6ec7f75d61ba24e31dab8a5
guilty-fred <fredrik@guilty.no>
2022-03-24 11:15:18 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vipps-login-mvp',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
